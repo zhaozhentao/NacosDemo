6a8e1e17ee26eccddf7339c94a35626fab53ecd6
zhaozhentao <qq344696734@gmail.com>
2022-07-03 21:00:14 +0800

    引入分布式事务

--- src/main/java/nacos/demo/DemoApplication.java
+++ src/main/java/nacos/demo/DemoApplication.java
@@ -1,11 +1,13 @@
 package nacos.demo;
 
+import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableAutoDataSourceProxy
 public class DemoApplication {
 
     public static void main(String[] args) {
