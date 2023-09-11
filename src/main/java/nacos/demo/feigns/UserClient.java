34fc89e9fc199a5f96dc8d4eac5723442772dd10
zhaozhentao <qq344696734@gmail.com>
2023-09-11 13:32:54 +0800

    修改client

--- src/main/java/nacos/demo/feigns/UserClient.java
+++ src/main/java/nacos/demo/feigns/UserClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("zzt")
+@FeignClient("zzt1")
 public interface UserClient {
 
     @RequestMapping("/user")
